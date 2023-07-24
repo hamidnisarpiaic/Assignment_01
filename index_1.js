35e269aa398487f6457fd2381ef371b79e9b2157
Hamid Nisar <hamidnisarpiaic@gmail.com>
2023-07-24 21:41:04 +0500

    just do now

---
+++ index_1.js
@@ -0,0 +1,5 @@
+// Exercise No. 2
+// declaring variable
+var nam = "Hamid";
+// displaying message
+console.log("Hello, ".concat(nam, "! Would you like to learn some python today?"));
---
+++ index_1.ts
@@ -0,0 +1,5 @@
+// Exercise No. 2
+// declaring variable
+let nam : string = "Hamid";
+// displaying message
+console.log(`Hello, ${nam}! Would you like to learn some python today?`)
