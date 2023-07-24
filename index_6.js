6b2fb8c5d310a757d29b70503db074a930693f41
Hamid Nisar <hamidnisarpiaic@gmail.com>
2023-07-24 22:09:36 +0500

    update it

---
+++ index_6.js
@@ -0,0 +1,15 @@
+function performOperations() {
+    // Addition: 4 + 4 = 8
+    var additionResult = 4 + 4;
+    console.log("Addition:", additionResult);
+    // Subtraction: 10 - 2 = 8
+    var subtractionResult = 10 - 2;
+    console.log("Subtraction:", subtractionResult);
+    // Multiplication: 2 * 4 = 8
+    var multiplicationResult = 2 * 4;
+    console.log("Multiplication:", multiplicationResult);
+    // Division: 16 / 2 = 8
+    var divisionResult = 16 / 2;
+    console.log("Division:", divisionResult);
+}
+performOperations();
---
+++ index_6.ts
@@ -0,0 +1,20 @@
+function performOperations(): void {
+    // Addition: 4 + 4 = 8
+    const additionResult = 4 + 4;
+    console.log("Addition:", additionResult);
+  
+    // Subtraction: 10 - 2 = 8
+    const subtractionResult = 10 - 2;
+    console.log("Subtraction:", subtractionResult);
+  
+    // Multiplication: 2 * 4 = 8
+    const multiplicationResult = 2 * 4;
+    console.log("Multiplication:", multiplicationResult);
+  
+    // Division: 16 / 2 = 8
+    const divisionResult = 16 / 2;
+    console.log("Division:", divisionResult);
+  }
+  
+  performOperations();
+  
