bfaac5ef4b9ed02f75ab41aa878e05334331bfc4
Hamid Nisar <hamidnisarpiaic@gmail.com>
2023-07-30 22:33:39 +0500

    update this.

--- index_27.js
+++ index_27.js
@@ -1,58 +1,33 @@
-console.log("Method");
-var favorite_fruits = ['banana', 'apple', 'mango'];
-// Method 1 - Using if statements
-if (favorite_fruits.includes('banana')) {
-    console.log('You really like bananas!');
+/*
+27.	Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+
+*/
+var alienColor1 = "green";
+if (alienColor1 === 'green') {
+    console.log('Players earned 5 points.');
 }
-if (favorite_fruits.includes('apple')) {
-    console.log('You really like apples!');
+else {
+    console.log('Player earned 10 points');
 }
-if (favorite_fruits.includes('mango')) {
-    console.log('You really like mangoes!');
+var alienColor2 = "yellow";
+if (alienColor2 === 'green') {
+    console.log('Players earned 5 points.');
 }
-console.log("\nAlternative method-2\n");
-// Method 2 - Using for...of loop
-for (var _i = 0, favorite_fruits_1 = favorite_fruits; _i < favorite_fruits_1.length; _i++) {
-    var f = favorite_fruits_1[_i];
-    if (f === 'banana') {
-        console.log('You really like bananas!');
-        break;
-    }
+else {
+    console.log('Player earned 10 points');
 }
-for (var _a = 0, favorite_fruits_2 = favorite_fruits; _a < favorite_fruits_2.length; _a++) {
-    var f = favorite_fruits_2[_a];
-    if (f === 'apple') {
-        console.log('You really like apples!');
-        break;
-    }
+var alienColor3 = "red";
+if (alienColor3 === 'green') {
+    console.log('Players earned 5 points.');
 }
-for (var _b = 0, favorite_fruits_3 = favorite_fruits; _b < favorite_fruits_3.length; _b++) {
-    var f = favorite_fruits_3[_b];
-    if (f === 'mango') {
-        console.log('You really like mangoes!');
-        break;
-    }
+else if (alienColor3 === "red") {
+    console.log('Player earned 15 points.');
 }
-console.log("\nAlternative method-3\n");
-// Method 3 - Using forEach loop and a mapping object
-var fruitMessages = {
-    'banana': 'You really like bananas!',
-    'apple': 'You really like apples!',
-    'mango': 'You really like mangoes!',
-};
-favorite_fruits.forEach(function (f) {
-    if (fruitMessages[f]) {
-        console.log(fruitMessages[f]);
-    }
-});
-console.log("\nAlternative method-4\n");
-// Method 4 - Using array indexing
-if (favorite_fruits[0] === 'banana') {
-    console.log('You really like bananas!');
-}
-if (favorite_fruits[1] === 'apple') {
-    console.log('You really like apples!');
-}
-if (favorite_fruits[2] === 'mango') {
-    console.log('You really like mangoes!');
+else {
+    console.log('Player earned 10 points.');
 }
--- index_27.ts
+++ index_27.ts
@@ -1,69 +1,34 @@
-console.log("Method");
-const favorite_fruits: string[] = ['banana', 'apple', 'mango'];
+/*
+27.	Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
 
-// Method 1 - Using if statements
-if (favorite_fruits.includes('banana')) {
-  console.log('You really like bananas!');
+
+*/
+let alienColor1: string = "green";
+
+if (alienColor1 === 'green') { 
+  console.log('Players earned 5 points.');
+} else {
+  console.log('Player earned 10 points'); 
 }
 
-if (favorite_fruits.includes('apple')) {
-  console.log('You really like apples!');
+let alienColor2: string = "yellow";
+
+if (alienColor2 === 'green') { 
+  console.log('Players earned 5 points.');
+} else {
+  console.log('Player earned 10 points'); 
 }
 
-if (favorite_fruits.includes('mango')) {
-  console.log('You really like mangoes!');
-}
+let alienColor3: string = "red";
 
-console.log("\nAlternative method-2\n");
-
-// Method 2 - Using for...of loop
-for (let f of favorite_fruits) {
-  if (f === 'banana') {
-    console.log('You really like bananas!');
-    break;
-  }
-}
-
-for (let f of favorite_fruits) {
-  if (f === 'apple') {
-    console.log('You really like apples!');
-    break;
-  }
-}
-
-for (let f of favorite_fruits) {
-  if (f === 'mango') {
-    console.log('You really like mangoes!');
-    break;
-  }
-}
-
-console.log("\nAlternative method-3\n");
-
-// Method 3 - Using forEach loop and a mapping object
-const fruitMessages: { [key: string]: string } = {
-  'banana': 'You really like bananas!',
-  'apple': 'You really like apples!',
-  'mango': 'You really like mangoes!',
-};
-
-favorite_fruits.forEach((f) => {
-  if (fruitMessages[f]) {
-    console.log(fruitMessages[f]);
-  }
-});
-
-console.log("\nAlternative method-4\n");
-
-// Method 4 - Using array indexing
-if (favorite_fruits[0] === 'banana') {
-  console.log('You really like bananas!');
-}
-
-if (favorite_fruits[1] === 'apple') {
-  console.log('You really like apples!');
-}
-
-if (favorite_fruits[2] === 'mango') {
-  console.log('You really like mangoes!');
+if (alienColor3 === 'green') { 
+  console.log('Players earned 5 points.');
+} else if (alienColor3 === "red") { 
+  console.log ('Player earned 15 points.');
+} else {
+  console.log ('Player earned 10 points.');
 }
