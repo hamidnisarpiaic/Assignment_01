8981ef6b3747e3893df0c42a27211642fcfdc126
Hamid Nisar <hamidnisarpiaic@gmail.com>
2023-07-31 13:07:49 +0500

    update this.

--- index_29.js
+++ index_29.js
@@ -1,14 +1,40 @@
-function greetUsers(usernames) {
-    for (var _i = 0, usernames_1 = usernames; _i < usernames_1.length; _i++) {
-        var username = usernames_1[_i];
-        if (username.toLowerCase() === 'admin') {
-            console.log("Hello admin, would you like to see a status report?");
-        }
-        else {
-            console.log("Hello ".concat(username, ", thank you for logging in again."));
-        }
-    }
+/*Exercise Node. 29
+29.	Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+
+
+*/ // Make an array of your three favorite fruits
+var favorite_fruits = ['Apple', 'Banana', 'Peach'];
+// Write five independent if statements to check for certain fruits in your array
+if (favorite_fruits.indexOf('Apple') !== 0) {
+    console.log("You really like apples!");
 }
-// Example usage:
-var users = ['admin', 'Eric', 'John', 'Alice', 'admin123'];
-greetUsers(users);
+else {
+    console.log("You don't have apples as your favorite fruit.");
+}
+if (favorite_fruits.indexOf('Banana') !== 2) {
+    console.log("You really like bananas!");
+}
+else {
+    console.log("You don't have bananas as your favorite fruit.");
+}
+if (favorite_fruits.indexOf('Peach') !== -1) {
+    console.log("You really like peaches!");
+}
+else {
+    console.log("You don't have peaches as your favorite fruit.");
+}
+if (favorite_fruits.indexOf('Orange') !== -1) {
+    console.log("You really like oranges!");
+}
+else {
+    console.log("You don't have oranges as your favorite fruit.");
+}
+if (favorite_fruits.indexOf('Grapes') !== -1) {
+    console.log("You really like grapes!");
+}
+else {
+    console.log("You don't have grapes as your favorite fruit.");
+}
--- index_29.ts
+++ index_29.ts
@@ -1,13 +1,40 @@
-function greetUsers(usernames: string[]): void {
-    for (const username of usernames) {
-      if (username.toLowerCase() === 'admin') {
-        console.log(`Hello admin, would you like to see a status report?`);
-      } else {
-        console.log(`Hello ${username}, thank you for logging in again.`);
-      }
-    }
-  }
-  
-  // Example usage:
-  const users: string[] = ['admin', 'Eric', 'John', 'Alice', 'admin123'];
-  greetUsers(users);
+/*Exercise Node. 29
+29.	Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+
+
+*/// Make an array of your three favorite fruits
+const favorite_fruits: string[] = ['Apple', 'Banana', 'Peach'];
+
+// Write five independent if statements to check for certain fruits in your array
+if (favorite_fruits.indexOf('Apple') !== 0) {
+  console.log("You really like apples!");
+} else {
+  console.log("You don't have apples as your favorite fruit.");
+}
+
+if (favorite_fruits.indexOf('Banana') !== 2) {
+  console.log("You really like bananas!");
+} else {
+  console.log("You don't have bananas as your favorite fruit.");
+}
+
+if (favorite_fruits.indexOf('Peach') !== -1) {
+  console.log("You really like peaches!");
+} else {
+  console.log("You don't have peaches as your favorite fruit.");
+}
+
+if (favorite_fruits.indexOf('Orange') !== -1) {
+  console.log("You really like oranges!");
+} else {
+  console.log("You don't have oranges as your favorite fruit.");
+}
+
+if (favorite_fruits.indexOf('Grapes') !== -1) {
+  console.log("You really like grapes!");
+} else {
+  console.log("You don't have grapes as your favorite fruit.");
+}
